738073227027cf3396e1a856f85726c0c0d87818
thomas <thomas@datastem.nl>
2022-12-31 16:44:35 +0000

    threading

--- threading/src/main.rs
+++ threading/src/main.rs
@@ -2,13 +2,17 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {} from the spawned thread!", i);
-            thread::sleep(Duration::from_millis(1));
-        }
+    let v = vec![1, 2, 3];
+
+    // move data in 
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
     });
 
+    // wait for spawned threads to finish
+    handle.join().unwrap();
+
+    // main thread actions
     for i in 1..5 {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
